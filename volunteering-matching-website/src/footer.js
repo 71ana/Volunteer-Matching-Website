93760edc3994852adbc4dea14c3a592b7f90c215
71ana <flavia.stegaru@student.upt.ro>
2023-11-18 12:56:08 +0200

    added some footer details

--- volunteering-matching-website/src/footer.js
+++ volunteering-matching-website/src/footer.js
@@ -1,16 +1,46 @@
 import React from "react";
 import "./footer.css";
-const Footer = () =>
-{
-   return(
-       <>
-           <section className='footer'>
-               <div className='text-center'>
-                    <a>Start Volunteering Today!</a>
-               </div>
-           </section>
-       </>
-   );
-}
+import { FaFacebook } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+
+const Footer = () => {
+    return (
+        <>
+            <section className="footer">
+                <div className="container">
+                    <div className="social text-left">
+                        <ul>
+                            <li>
+                                <a href="#" className="icon-link">
+                                    <FaFacebook />
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" className="icon-link">
+                                    <FaInstagram />
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" className="icon-link">
+                                    <FaTwitter />
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <p className="text-center rights-text">
+                        Contact Us
+                    </p>
+
+                    <div className="text-right">
+                        <h2>Start Volunteering Today!</h2>
+                    </div>
+                </div>
+            </section>
+        </>
+    );
+};
+
 
 export default Footer
